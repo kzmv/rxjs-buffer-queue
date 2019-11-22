e01ef38d5595d4f8790d7da85432fbb593e6d922
kzmv <kouzmov@gmail.com>
2019-11-22 15:47:28 +0000

    2 files modified

--- index.ts
+++ index.ts
@@ -3,14 +3,26 @@ import './style.css';
 import { buffer } from 'rxjs/operators';
 import { interval, Subject, BehaviorSubject, combineLatest } from 'rxjs';
 import { filter } from 'rxjs/operators';
-
+import _ from "lodash";
 
 const ticker$ = interval(500);
 const isProcessing$ = new BehaviorSubject<boolean>(true);
 const transactions$ = new Subject<any>();
-
+const data$ = new BehaviorSubject<any>([]);
 
 const transactions = [
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
+  {type: 'add', data: [{'test': 'test1'}]},
   {type: 'add', data: [{'test': 'test1'}]},
   {type: 'add', data: [{'test': 'test1'}]},
   {type: 'add', data: [{'test': 'test1'}]},
@@ -20,16 +32,32 @@ const transactions = [
 for(let i =0 ; i< transactions.length;i++) {
   setTimeout(() => {
     transactions$.next(transactions[i]);
-  }, i*300);
+  }, i*130);
 }
 
 const trigger = combineLatest(ticker$,isProcessing$).pipe(filter(([_,isProcessing]) => isProcessing))
 //
 transactions$.pipe(
-  buffer(trigger)
-).subscribe(data => {
-  console.log('Injest:')
-  console.log(data);
+  buffer(trigger),
+  filter(transactions => transactions.length > 0)
+).subscribe(trans => {
+  console.log('Injest Transactions:')
+  console.log(trans);
+  
+  isProcessing$.next(false);
+  setTimeout(() => {
+    data$.next(_.flatten(trans.map(transaction => transaction.data)));
+  }, 1000)
 })
 
-console.log('test');
+data$.subscribe(data => {
+  console.log('Data:');
+  console.log(data);
+  isProcessing$.next(true);
+})
+
+for(let i =0 ; i< transactions.length;i++) {
+  setTimeout(() => {
+    transactions$.next(transactions[i]);
+  }, i*1000);
+}
